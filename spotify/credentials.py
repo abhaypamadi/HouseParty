a3ebccc855f557ac1bceb0cd658f510447ef1b21
Abhay M Pamadi <70917073+abhaypamadi@users.noreply.github.com>
2022-08-19 22:43:04 +0530

    Update credentials.py

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,3 @@
-CLIENT_ID = "e7cec86a29cb4c6e956ff3a11c29a565"
-CLIENT_SECRET = "fead05db8d17430eb560fa46db9f3828"
+CLIENT_ID = ""
+CLIENT_SECRET = ""
 REDIRECT_URI = "https://houseparty-music.herokuapp.com/spotify/redirect"
